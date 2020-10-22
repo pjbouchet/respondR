5da6a52f5ea5487f736d3b71e52b1ce85ba6771c
Phil Bouchet <pjbouchet@gmail.com>
2020-10-22 08:31:02 +0100

    Update create_doc

--- R/create_doc.R
+++ R/create_doc.R
@@ -1,27 +1,32 @@
-#' Copy the Microsoft Word template document
+#' Copy/find the Microsoft Word template document
 #'
-#' @param rstudio Logical. Whether the function is being called from within RStudio. Defaults to TRUE.
-#' @param destination Destination folder. If \code{rstudio = TRUE}, the file will be saved in the same directory as the current file. If \code{rstudio = TRUE} and \code{destination} is not specified, will default to the current working directory.
+#' @param output.dir Destination folder. If \code{rstudio = TRUE}, the file will be saved in the same directory as the current file. If \code{rstudio = TRUE} and \code{destination} is not specified, will default to the current working directory.
 #' @param file.name Name of the output file. If not specified, will default to `response_template.docx`.
-#' 
+#' @param rstudio Logical. Whether the function is being called from within RStudio. Defaults to TRUE.
 #' @return A LaTeX formatted string showing text differences when rendered.
 #' @author Phil J. Bouchet
 #' @export
 
-create_doc <- function(rstudio = TRUE, destination = NULL, file.name = NULL){
-
+create_doc <- function(output.dir = NULL, file.name = NULL, rstudio = TRUE){
+  
   # Directory
   if (rstudio) {
-    if (is.null(destination)) dir_path <- dirname(rstudioapi::getSourceEditorContext()$path) else dir_path <- destination
+    if (is.null(output.dir)) dir_path <- dirname(rstudioapi::getSourceEditorContext()$path) else dir_path <- output.dir
   } else {
-    if (is.null(destination)) dir_path <- getwd() else dir_path <- destination
+    if (is.null(output.dir)) dir_path <- getwd() else dir_path <- output.dir
   }
 
   # File name
   if (is.null(file.name)) file.name <- "response_template.docx"
   if(!grepl(pattern = ".docx", x = file.name)) file.name <- paste0(file.name, ".docx")
   
-  # Copy (and rename) file
-  if(!file.exists(file.path(dir_path, file.name))) file.copy(from = system.file("inst/response_template.docx", package = "respondR"), to = file.path(dir_path, tolower(file.name)))
+  # Full path
+  filepath <- file.path(dir_path, file.name)
+  
+  # Make a copy of the template if necessary
+  if(file.exists(filepath)){ cat("A file with this name already exists. Using version on disk.")
+  } else { file.copy(from = system.file("inst/response_template.docx", package = "respondR"), 
+              to = tolower(filepath), overwrite = FALSE)}
 
-}
+  return(filepath)
+} 
